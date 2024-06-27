d0d08ca05badbbd8a8c219710e92f12b71b8aedf
nebulaa44 <gmdmir372@gmail.com>
2024-06-27 16:34:57 -0500

    document yanc::user::username()

--- src/user.hpp
+++ src/user.hpp
@@ -1,7 +1,6 @@
 // user.hpp
 // Retrieves information about current user
 
-
 #ifndef YANC_USER_HPP
 #define YANC_USER_HPP
 
@@ -11,6 +10,7 @@ namespace yanc
 {
     namespace user
     {
+        /// Returns the username of the user calling yanc
         std::string_view username();
     }
 }
